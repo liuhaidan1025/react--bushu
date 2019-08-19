bb31b363b1bb8779eedc2ffef9fc847ee5d843a0
liuhaidan1025 <80543957@qq.comgit config --global user.name  liuhaidan1025git config --global user.email 80543957@qq.com>
2019-08-19 15:44:14 +0800

    gh-pages

---
+++ precache-manifest.0b83a7c70559db998d5f0504d3b3799e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d382ed2ccdad0d3a3e7815588080de0f",
+    "url": "/react--bushu/index.html"
+  },
+  {
+    "revision": "b2ac54e52d3dd7629189",
+    "url": "/react--bushu/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "0681e5bb692bcc9416aa",
+    "url": "/react--bushu/static/js/2.ed64165c.chunk.js"
+  },
+  {
+    "revision": "b2ac54e52d3dd7629189",
+    "url": "/react--bushu/static/js/main.6ff56fe2.chunk.js"
+  },
+  {
+    "revision": "aa71b4a5d5f4936a7b01",
+    "url": "/react--bushu/static/js/runtime~main.efc0a443.js"
+  }
+]);
